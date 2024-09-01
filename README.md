44c70892f9323567ed72c884e7ea8c4bfdb959c7
John T. Foster <johntfosterjr@gmail.com>
2024-09-01 18:53:37 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Assignment 2
+
+![Assignment 2](https://github.com/PGE323M/assignment2-solution/workflows/.github/workflows/main.yml/badge.svg)
+
+Open the [Assignment 2 Jupyter Notebook](assignment2.ipynb) and follow the instructions.
+
+## Testing
+
+If you would like to check to see if your solution is correct, run the following commands at the Terminal command line
+
+````
+python test.py
+````
---
+++ test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# Copyright 2018-2020 John T. Foster
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+import nbconvert
+
+with open("assignment2.ipynb") as f:
+    exporter = nbconvert.PythonExporter()
+    python_file, _ = exporter.from_file(f)
+
+
+with open("assignment2.py", "w") as f:
+    f.write(python_file)
+
+
+from assignment2 import square, finite_diff, well
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_square(self):
+        assert square(10) == 100
+        assert square(0) == 0
+
+    def test_finite_diff(self):
+        assert abs(finite_diff(4, 0.1) - 5.0) < 1e-6
+
+    def test_well(self):
+        assert abs(well(1.2, 5000, 3000) - 427.44) <= 0.1 
+
+    def test_square_private(self):
+        assert square(-2) == 4
+
+    def test_finite_diff_private(self):
+        assert abs(finite_diff(4, 0.001) - 5.0) < 1e-6
+
+    def test_well_private(self):
+        assert abs(well(0.9, 5000) - 480.87) <= 0.1
+
+
+if __name__ == '__main__':
+    unittest.main()
